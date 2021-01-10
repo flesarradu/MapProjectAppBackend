f5e0b3c3f82457c5110e164a5a22e6a823e5155d
Flesar Radu <32610325+flesarradu@users.noreply.github.com>
2021-01-11 01:48:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Users'); // Create a table for 'User' with default settings
-
+mobileApp.tables.add('Places');
+mobileApp.tables.add('Cases');
+mobileApp.tables.add('Reviews');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
